16b91b2c8396874015714ea07385ede039991fc9
Kondisetty Sai Dheeraj <105906908+kondisettysaidheeraj@users.noreply.github.com>
2022-10-06 16:51:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+ import { Tictactoe } from './Tictactoe';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Tictactoe />
+    
+    </div>
+  );
+}
+
+export default App;
